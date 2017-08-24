ff0cd1d45d7e81caf2d1d65ef0366ce47aa8e95e
jzc <justincai.lps@gmail.com>
2017-08-23 23:43:46 -0600

    added user model and some tenative due layout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import math
 
 from flask import Flask, render_template, redirect, url_for
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -9,7 +10,6 @@ from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import Required
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['DEBUG'] = True
@@ -20,6 +20,10 @@ db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Purchase=Purchase)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
 class AddForm(FlaskForm):
     item = StringField("Item", validators=[Required()])
     by = StringField("By", validators=[Required()])
@@ -27,17 +31,25 @@ class AddForm(FlaskForm):
     price = FloatField("Price", validators=[Required()])
     submit = SubmitField("Submit")
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    dues = db.Column(db.Integer)
+    def __repr__(self):
+        return "<User (%s, %s, %s)>" % (self.id, self.name, self.dues)
 
 class Purchase(db.Model):
     __tablename__ = "purchases"
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(64))
-    by = db.Column(db.String(64))
+    by_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    by = db.relationship("User")
     split = db.Column(db.Integer)
     price = db.Column(db.Float)
 
     def __repr__(self):
-        return "(%s, %s, %s, %s, %s)" % (self.id, self.item, self.by, self.split, self.price)
+        return "<Purchase (%s, %s, %s, %s, %s)>" % (self.id, self.item, self.by.name, self.split, self.price)
 
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -51,8 +63,12 @@ def index():
         form.price.data = ""
         db.session.add(p)
         db.session.commit()
+        #new item add animation
         return redirect(url_for("index"))
-    return render_template("main.html", purchases=Purchase.query.all(), form=form)
+    users = User.query.all()
+    n_users = len(users)
+    n_rows = math.ceil(n_users/3.0)
+    return render_template("main.html", purchases=Purchase.query.all(), form=form, n_rows=n_rows, users=users, n_users=n_users)
 
 if __name__ == "__main__":
     manager.run()
