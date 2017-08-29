ebcfb1f6fe98648f4ba025922a4fb48ec85291bb
jzc <justincai.lps@gmail.com>
2017-08-28 21:59:12 -0600

    added user registration

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_migrate import Migrate, MigrateCommand
 from wtforms import StringField, SubmitField, PasswordField, SelectMultipleField, BooleanField
-from wtforms.validators import Required, ValidationError
+from wtforms.validators import Required, ValidationError, EqualTo, Regexp
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -18,16 +20,26 @@ app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "key"
+app.config["SERVER_NAME"] = "192.168.1.15" #raspberry ip
 db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 login_manager = LoginManager(app)
+migrate = Migrate(app, db)
 login_manager.login_view = "login"
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Purchase=Purchase, Due=Due)
 
+@manager.command
+def create_registration_link():
+    new_user = User()
+    db.session.add(new_user)
+    db.session.commit()
+    print(url_for("register", token=new_user.generate_registration_token(), _external=True))
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -53,12 +65,26 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Keep me logged in")
     submit = SubmitField("Log in")
 
+class RegistrationForm(FlaskForm):
+    name = StringField("Name", validators=[Required(), Regexp(r"^[A-Za-z]+$", message="Username must only have letters.")])
+    password = PasswordField("Password", validators=[Required()])
+    confirm = PasswordField("Confirm password", validators=[Required(), EqualTo("password", message="Passwords must match.")])
+    submit = SubmitField("Create account")
+
+    def validate_name(self, field):
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError("Name is already in use.")
+
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     dues = db.relationship("Due", foreign_keys="Due.owner_id", backref="owner")
     password_hash = db.Column(db.String(128))
+
+    def generate_registration_token(self):
+        s = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+        return s.dumps(self.id)
 
     @property
     def password(self):
@@ -96,6 +122,30 @@ class Due(db.Model):
     def __repr__(self):
         return "<Due %s, %s (%s)>" % (self.owner_id, self.to_id, self.amount)
 
+@app.route("/register/<token>", methods=["POST","GET"])
+def register(token):
+    s = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+    try:
+        id = s.loads(token, max_age=86400)
+    except:
+        flash("The registration link is invalid or has expired")
+        return redirect(url_for("index"))
+
+    u = User.query.get(id)
+    if u.name and u.password_hash:
+        flash("User is already registered.")
+        return redirect(url_for("index"))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        u.name = form.name.data
+        u.password = form.password.data
+        db.session.add(u)
+        db.session.commit()
+        flash("User registration successful.")
+        return redirect(url_for("index"))
+    return render_template("quick_form.html", header="Register", form=form)
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     form = LoginForm()
@@ -106,7 +156,7 @@ def login():
             login_user(user, form.remember_me.data)
             return redirect(request.args.get("next") or url_for("index"))
         flash("Invalid username or password.")
-    return render_template("login.html", form=form)
+    return render_template("quick_form.html", header="Log in", form=form)
 
 @app.route("/logout")
 @login_required
@@ -146,7 +196,8 @@ def flash_errors(form):
 @app.route("/", methods=["GET","POST"])
 def index():
     form = AddPurchaseForm()
-    form.split.choices = [(u.id, u.name) for u in User.query.filter(User.name != current_user.name)]
+    if current_user.is_authenticated:
+        form.split.choices = [(u.id, u.name) for u in User.query.filter(User.name != current_user.name)]
     if form.validate_on_submit():
         split = ", ".join([User.query.get(id).name for id in form.split.data])
         p = Purchase(item=form.item.data, 
@@ -161,7 +212,7 @@ def index():
         return redirect(url_for("index"))
 
     flash_errors(form)
-    users = User.query.all()
+    users = User.query.filter(User.name != None).all()
     n_users = len(users)
     n_rows = math.ceil(n_users/3.0)
     return render_template("index.html", purchases=Purchase.query.all(), form=form, n_rows=n_rows, users=users, n_users=n_users)
